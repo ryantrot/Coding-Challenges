76233dbfd8cab2863ee26b5e77f7002466053ea6
ryantrot <51455606+ryantrot@users.noreply.github.com>
2020-03-16 02:13:10 -0500

    Add files via upload

---
+++ SharpEcho.Recruiting.SpellChecker.Core/DictionaryDotComSpellChecker.cs
@@ -0,0 +1,69 @@
+﻿using SharpEcho.Recruiting.SpellChecker.Contracts;
+using System;
+using System.IO;
+using System.Net;
+
+namespace SharpEcho.Recruiting.SpellChecker.Core
+{
+    /// <summary>
+    /// This is a dictionary based spell checker that uses dictionary.com to determine if
+    /// a word is spelled correctly
+    /// 
+    /// The URL to do this looks like this: http://dictionary.reference.com/browse/<word>
+    /// where <word> is the word to be checked
+    /// 
+    /// Example: http://dictionary.reference.com/browse/SharpEcho would lookup the word SharpEcho
+    /// 
+    /// We look for something in the response that gives us a clear indication whether the
+    /// word is spelled correctly or not
+    /// </summary>
+    public class DictionaryDotComSpellChecker : ISpellChecker
+    {
+
+        public bool Check(string word)
+        {
+            //throw new System.NotImplementedException();
+
+            bool found = false;
+            string receiveContent = string.Empty;
+
+
+            /////////////////////////////////////////////////
+            /// I had to place this block of code because I was receiving an error saying the program could not create an SSL/TLS secure channel 
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                   | SecurityProtocolType.Tls11
+                   | SecurityProtocolType.Tls12
+                   | SecurityProtocolType.Ssl3;
+            ///////////////////////////////////////////////////
+
+            string requestUrl = @"https://dictionary.reference.com/browse/" + word;
+
+            var request = (HttpWebRequest)WebRequest.Create(requestUrl);
+            //throw new System.NotImplementedException();
+            try
+            {
+                //checking if we get data as a response.
+                using (WebResponse response = request.GetResponse())
+                {
+                    // gets the response
+                    var responseStream = response.GetResponseStream();
+                    //makes sure the response it not empty 
+                    if (responseStream != null)
+                    {
+                        //if not empty return true
+                        found = true;
+                    }
+                }
+                
+            }
+            //if an exception is reached that means the an error was returned meaning the word was missed spelled
+            catch (Exception e)
+            {
+                //return false stating the word was misspelled
+                found = false;
+            }
+            return found;
+        }
+    }
+}
---
+++ SharpEcho.Recruiting.SpellChecker.Core/MnemonicSpellCheckerIBeforeE.cs
@@ -0,0 +1,36 @@
+﻿using SharpEcho.Recruiting.SpellChecker.Contracts;
+
+namespace SharpEcho.Recruiting.SpellChecker.Core
+{
+    /// <summary>
+    /// This spell checker implements the following rule:
+    /// "I before E, except after C" is a mnemonic rule of thumb for English spelling.
+    /// If one is unsure whether a word is spelled with the sequence ei or ie, the rhyme
+    /// suggests that the correct order is ie unless the preceding letter is c, in which case it is ei. 
+    /// 
+    /// Examples: believe, fierce, collie, die, friend, deceive, ceiling, receipt would be evaulated as spelled correctly
+    /// heir, protein, science, seeing, their, and veil would be evaluated as spelled incorrectly.
+    /// </summary>
+    public class MnemonicSpellCheckerIBeforeE : ISpellChecker
+    {
+        /// <summary>
+        /// Returns false if the word contains a letter sequence of "ie" when it is immediately preceded by c
+        /// </summary>
+        /// <param name="word">The word to be checked</param>
+        /// <returns>true when the word is spelled correctly, false otherwise</returns>
+        public bool Check(string word)
+        {
+            //throw new System.NotImplementedException();
+            //check if the word contains cie
+            if (word.Contains("cie"))
+            {
+                //if so, it is misspelled and should return false  
+                return false;
+            }
+            else 
+            {
+                return true;
+            }
+        }
+    }
+}
