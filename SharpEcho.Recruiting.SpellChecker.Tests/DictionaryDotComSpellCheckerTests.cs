d1298761e693649749d3c27cb9969b2892e4202f
ryantrot <51455606+ryantrot@users.noreply.github.com>
2020-03-16 02:15:21 -0500

    Add files via upload

---
+++ SharpEcho.Recruiting.SpellChecker.Tests/DictionaryDotComSpellCheckerTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+using SharpEcho.Recruiting.SpellChecker.Contracts;
+using SharpEcho.Recruiting.SpellChecker.Core;
+using System.Net;
+
+namespace SharpEcho.Recruiting.SpellChecker.Tests
+{
+    [TestFixture]
+    class DictionaryDotComSpellCheckerTests
+    {
+        private ISpellChecker SpellChecker;
+
+        [OneTimeSetUp]
+        public void TestFixureSetUp()
+        {
+            SpellChecker = new DictionaryDotComSpellChecker();
+        }
+
+        [Test]
+        public void Check_That_SharpEcho_Is_Misspelled()
+        {
+            // implement this test
+            string teststr = "SharpEcho";
+            SpellChecker.Check(teststr);
+        }
+
+        [Test]
+        public void Check_That_South_Is_Not_Misspelled()
+        {
+            // implement this test
+            string teststr = "South";
+            SpellChecker.Check(teststr);
+        }
+    }
+}
---
+++ SharpEcho.Recruiting.SpellChecker.Tests/MnemonicSpellCheckerIBeforeETests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+
+
+
+using SharpEcho.Recruiting.SpellChecker.Contracts;
+using SharpEcho.Recruiting.SpellChecker.Core;
+
+namespace SharpEcho.Recruiting.SpellChecker.Tests
+{
+
+    [TestFixture]
+    class MnemonicSpellCheckerIBeforeETests
+    {
+        private ISpellChecker SpellChecker;
+
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
+        {
+            SpellChecker = new MnemonicSpellCheckerIBeforeE();
+        }
+
+        [Test]
+        public void Check_Word_That_Contains_I_Before_E_Is_Spelled_Correctly()
+        {
+            /// Examples: believe, fierce, collie, die, friend, deceive, ceiling, receipt would be evaulated as spelled correctly
+            /// heir, protein, science, seeing, their, and veil would be evaluated as spelled incorrectly.
+            // implement this test
+            SpellChecker.Check("Fierce");
+
+        }
+
+        [Test]
+        public void Check_Word_That_Contains_I_Before_E_Is_Spelled_Incorrectly()
+        {
+            // implement this test
+            SpellChecker.Check("decieve");
+        }      
+    }
+
+  
+}
